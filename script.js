a2a451aaf2b3c076a05a137e7abcd7fd2126f5ad
Alek Klem <a.klem@students.uu.nl>
2026-06-07 16:20:30 +0200

    Initial website

---
+++ script.js
@@ -0,0 +1,34 @@
+const lightbox = document.querySelector(".lightbox");
+const lightboxImage = lightbox.querySelector("img");
+const closeButton = lightbox.querySelector("button");
+
+function openLightbox(image) {
+  lightboxImage.src = image.src;
+  lightboxImage.alt = image.alt;
+  lightbox.hidden = false;
+  closeButton.focus();
+}
+
+function closeLightbox() {
+  lightbox.hidden = true;
+  lightboxImage.removeAttribute("src");
+  lightboxImage.alt = "";
+}
+
+document.querySelectorAll("[data-lightbox]").forEach((image) => {
+  image.addEventListener("click", () => openLightbox(image));
+});
+
+closeButton.addEventListener("click", closeLightbox);
+
+lightbox.addEventListener("click", (event) => {
+  if (event.target === lightbox) {
+    closeLightbox();
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape" && !lightbox.hidden) {
+    closeLightbox();
+  }
+});
